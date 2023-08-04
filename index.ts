e9f3b2bac5e9cb0c3b511f079919e48b93fd6a3c
Caner <caner.iskenderoglu@gmx.de>
2023-08-04 22:41:36 +0200

    generel setup

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import AppError from "./utils/appError";
+import { globalErrHandler } from "./controllers/errorController";
+
+dotenv.config();
+
+export const app: Express = express();
+//1. Midldewares
+//Middlware order in code is important .defines execution order -> needs be to always before othes routes
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// 3) Routers mouting
+/*
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+*/
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrHandler);
+
+//index.js mainly used for connecting our different middlewares
